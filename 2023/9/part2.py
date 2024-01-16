1355c66e43ab601bc5d732934b8f9fc08b7bc932
Gábor Major <majorg.96@gmail.com>
2024-01-16 16:56:44 +0200

    ninth challenge

---
+++ 2023/9/part2.py
@@ -0,0 +1,17 @@
+
+total = 0
+
+def predict_next_value(values) -> int:
+
+    if all(v == 0 for v in values):
+        return 0
+
+    return values[0] - predict_next_value([j-i for i, j in zip(values[:-1], values[1:])])
+
+
+with open("2023/9/input.txt", "r") as file:
+    for line in file:
+        values = [int(i) for i in line.split()]
+        total = total + predict_next_value(values)
+
+print(total)
